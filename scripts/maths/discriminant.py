634cbc0a377744b32f45515f6277900d1f45cb6f
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2020-11-20 11:03:52 +0100

    Update discriminant.py

--- scripts/maths/discriminant.py
+++ scripts/maths/discriminant.py
@@ -1,4 +1,4 @@
-from mathsup import disc
+from MathSup import disc
 from math import sqrt
 
 
@@ -8,10 +8,10 @@ c = float(input("c = "))
 
 d = disc(a, b, c)
 if (d > 0):
-    print("x1 = ", (-b-sqrt(d))/(2*a))
-    print("x2 = ", (-b+sqrt(d))/(2*a))
+    print("x1 = (-" + str(b) + "-sqrt(" + str(d) + "))/(2*" + str(a) + ")\n= " + str((-b-sqrt(d))/(2*a)))
+    print("x2 = (-" + str(b) + "+sqrt(" + str(d) + "))/(2*" + str(a) + ")\n= " + str((-b+sqrt(d))/(2*a)))
 elif d==0:
-    print("x = ", -b/(2*a))
+    print("x = -" + str(b) + "/(2*" + str(a) + ")\n= " + str(-b/(2*a)))
 elif (d < 0):
-    print("z1 = ", (-b-sqrt(-d))/(2*a))
-    print("z2 = ", (-b+sqrt(-d))/(2*a))
+    print("z1 = (-" + str(b) + "-i*sqrt(-(" + str(d) + ")))/(2*" + str(a) + ")\n= " + str((-b-sqrt(-d))/(2*a)))
+    print("z2 = (-" + str(b) + "-i*sqrt(-(" + str(d) + ")))/(2*" + str(a) + ")\n= " + str((-b+sqrt(-d))/(2*a)))
