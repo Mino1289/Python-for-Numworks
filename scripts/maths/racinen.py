f7b46a9362dd5dd9668633b0c4469a497ba975fa
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2020-12-06 12:22:52 +0100

    Update racinen.py
    Add round(x), to facilitate the reading on the calculator.

--- scripts/maths/racinen.py
+++ scripts/maths/racinen.py
@@ -1,10 +1,11 @@
-from cmath import cos, pi, e, sin, sqrt
+from cmath import cos, pi, sin, sqrt
 
 j = sqrt(-1)
 print("Donner la racine nième\nTel que Z^n = 1")
 n = int(input("n = "))
-print("Toutes les valeurs de solution de Z^" + str(n) + "= 1 sont :\n")
+print("Toutes les valeurs de solution de Z^" + str(n) + " = 1 sont :\n")
 for k in range(0, n):
-    alge = complex(cos((2*k*pi)/n), sin((2*k*pi)/n))
+    z = complex(cos((2*k*pi)/n),(sin((2*k*pi)/n)))
+    alge = round(z.real, 1) + round(z.imag, 1) * 1j
     print("z" + str(k+1) + " = cos(" + str(2*k) + "pi/" + str(n) + ") + i*sin(" + str(2*k) + "pi/" + str(n) + ")\n   = " + str(alge) + " \n")
     k += 1
