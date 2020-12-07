3d2a6b2d99ed49486c6d72e10d4563745d4b5292
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2020-12-07 20:44:35 +0100

    Update racinen.py
    Adding some new feature , that make easy to use

--- scripts/maths/racinen.py
+++ scripts/maths/racinen.py
@@ -1,10 +1,11 @@
-from cmath import cos, pi, sin
+from cmath import cos, pi, sin, sqrt
 
+j = sqrt(-1)
 print("Donner la racine nième\nTel que Z^n = 1")
 n = int(input("n = "))
 print("Toutes les valeurs de solution de\nZ^" + str(n) + " = 1 sont :\n")
 for k in range(0, n):
     z = complex(cos((2*k*pi)/n),(sin((2*k*pi)/n)))
     alge = round(z.real, 1) + round(z.imag, 1) * 1j
-    print("z" + str(k+1) + "= cos(" + str(2*k) + "pi/" + str(n) + ")+i*sin(" + str(2*k) + "pi/" + str(n) + ")\n   = " + str(alge) + " \n")
+    print("z" + str(k+1) + "= cos(" + str(2*k) + "pi/" + str(n) + ")+i*sin(" + str(2*k) + "pi/" + str(n) + ")\n   = " + str(alge).replace("j", "i") + " \n")
     k += 1
