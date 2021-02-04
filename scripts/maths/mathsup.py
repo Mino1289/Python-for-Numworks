bce895801880f3bd32056c38df7db78c8533a306
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2021-02-04 21:47:30 +0100

    Update mathsup.py

--- scripts/maths/mathsup.py
+++ scripts/maths/mathsup.py
@@ -4,13 +4,13 @@ def fact(x):
     """
     r = 1
     for i in range(x):
-        r = r * (i + 1)
+        r *= (i + 1)
     return int(r)
 
 
 def combi(n, k):
     """
-    n, k = int\n\nReturn k among n
+    n, k = int\n\nReturn k among n.
     """
     x = fact(n) / (fact((n - k)) * fact(k))
     return int(x)
@@ -18,7 +18,7 @@ def combi(n, k):
 
 def arran(n, k):
     """
-    n, k = int\n\nReturn arrangement of k objects among n
+    n, k = int\n\nReturn arrangement of k objects among n.
     """
     if k > n:
         return
@@ -36,53 +36,58 @@ def permut(n):
 
 def disc(a, b, c):
     """
-    a, b, c = int or float\n\nReturn b^2-4ac
+    a, b, c = int or float\n\nReturn b^2-4ac.
     """
     d = (b**2)-(4*a*c)
     return int(d)
 
-def q(a,b):
+
+def q(a, b):
     """
-    a & b = int\n\nReturn q in a = b*q+r
+    a & b = int\n\nReturn q in a = b*q+r.
     """
     q = a//b
-    r = a%b
+    r = a % b
     if r > b or r < 0:
         r -= b
         q += 1
     return int(q)
 
-def r(a,b):
+
+def r(a, b):
     """
-    a & b = int\n\nReturn r in a = b*q+r
+    a & b = int\n\nReturn r in a = b*q+r.
     """
-    r = a%b
+    r = a % b
     if r > b:
         r -= b
     elif r < 0:
         r += b
     return int(r)
 
-def dive(a,b):
+
+def dive(a, b):
     """
-    a & b = int\n\nReturn q & r in a = b*q+r
+    a & b = int\n\nReturn q & r in a = b*q+r.
     """
-    qu = q(a,b)
-    re = r(a,b)
+    qu = q(a, b)
+    re = r(a, b)
     return qu, re
 
-def iscong(a,b,n):
-    """
-    a, b, n = int\n\nReturn bool if a is congruent to b modulo n
-    """
-    return bool((a-b)%n == 0)
 
-def cong(a,n):
+def iscong(a, b, n):
     """
-    a,n = int\n\nReturn b & c the two closer to 0. Where a is congruent to b modulo n, and a is congruent to c modulo n.
+    a, b, n = int\n\nReturn bool if a is congruent to b modulo n.
     """
-    b = r(a,n)
-    if b==0:
+    return bool(r(a,b) == 0)
+
+
+def cong(a, n):
+    """
+    a, n = int\n\nReturn b & c the two closer to 0. Where a is congruent to b modulo n, and a is congruent to c modulo n.
+    """
+    b = r(a, n)
+    if b == 0:
         return int(b)
     else:
         if b < 0 or n < 0:
@@ -90,3 +95,48 @@ def cong(a,n):
         else:
             c = b-n
     return b, c
+
+
+def pgcd(a, b):
+    """
+    a, b = int\n\nReturn int, the greatest common divisor of a and b.
+    """
+    while (b > 0):
+        re = r(a, b)
+        a, b = b, re
+    return a
+
+
+def ppcm(a, b):
+    """
+    a, b = int\n\nReturn int, the least common divisor of a and b.
+    """
+    if type(a+b) != int:
+        return None
+    return q(a*b, pgcd(a, b))
+
+
+def prime(n):
+    """
+    n = int\n\nReturn bool if n is a prime number.
+    """
+    if type(n) != int or n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if r(n,i) == 0:
+            return False
+    return True
+
+def factor(n):
+    """
+    n = int\n\nReturn the prime factorization of n.
+    """
+    L = []
+    div = 2
+    while n > 1:
+        if r(n,div) == 0:
+            L.append(div)
+            n /= div 
+        else:
+            div += 1
+    return L
