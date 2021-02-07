14ab7a15ec9ae04384b3da4169f76dc663989b39
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2021-02-07 19:27:27 +0100

    Update congtest.py

--- scripts/maths/congtest.py
+++ scripts/maths/congtest.py
@@ -1,11 +1,11 @@
-from mathsup import cong
+from mathsup import cong,r
 
-print("Donnez l'expression d'exposant n"+
+print("Donnez l'expression\nd'exposant n"+
 "\nSans le n : 7 est 7^n"+
 "\nEt 7^2n = 49^n soit 49")
-exp = int(input(""))
+exp = int(input("exp = "))
 print("Donnez le modulo :")
-mod = int(input(""))
+mod = int(input("mod = "))
 print("Résultats :")
-for n in range(1, mod):
-    print(str(exp) + "^" + str(n) + " iscong : " + str(cong(exp**n,mod)))
+for n in range(1,min(exp+1, mod+1)):
+    print(str(exp) + "^" + str(n) + " est congru à : " + str(r(exp**n,mod)) + " [" + str(mod)+"]")
