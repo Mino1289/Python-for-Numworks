d4a438dd6fb73e123544030ffc293d05cb423e5d
Mino1289 <68814671+Mino1289@users.noreply.github.com>
2021-03-21 21:23:25 +0100

    Update congtest.py

--- scripts/maths/congtest.py
+++ scripts/maths/congtest.py
@@ -15,7 +15,7 @@ if s == 1:
     mod = int(input("mod = "))
     print("Résultats :")
     for x in range(1, max(exp+1,mod+1)):
-        print(str(exp)+"*"+str(x)+ "+"+ str(pls) + "="+ str(exp*x+pls) +" est congru à : "+ str(r(exp*x, mod)) + " [" + str(mod)+"]")
+        print(str(exp)+"*"+str(x)+ "+"+ str(pls) + "="+ str(exp*x+pls) +" ≡ : "+ str(r(exp*x, mod)) + " [" + str(mod)+"]")
 
 elif s == 2:
     #n en exposant
@@ -23,10 +23,14 @@ elif s == 2:
     "\nSans le n : 7 est 7^n"+
     "\nEt 7^2n = 49^n soit 49")
     exp = int(input("= "))
+    print("Donnez la suite de l'expression,\nde facteur n sans le n: \n7 est 7*n")
+    fac = int(input("= "))
+    print("Valeur en + :")
+    pls = int(input("= "))
     print("Donnez le modulo :")
     mod = int(input("mod = "))  
     print("Résultats :")
     for n in range(1,max(exp+1, mod+1)):
-        print(str(exp) + "^" + str(n) + " est congru à : " + str(r(exp**n,mod)) + " [" + str(mod)+"]")
+        print(str(exp) + "^" + str(n)+"+"+str(fac)+"*"+ str(n) + "+"+str(pls) + " ≡ " + str(r(exp**n+fac*n+pls,mod)) + " [" + str(mod)+"]")
 else:
     print("t'es con ou quoi ?")
