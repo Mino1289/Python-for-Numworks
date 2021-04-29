62bd68d7ca9b069f02bbbb11f2c0a360150ab62c
unknown <kretzvictor@orange.fr>
2021-04-29 22:54:09 +0200

    Valeur à la fin corrigé

--- scripts/maths/pgcd.py
+++ scripts/maths/pgcd.py
@@ -2,10 +2,10 @@ from mathsup import r, q, pgcd
 
 print("PGCD de :")
 a, b = abs(int(input("a = "))), abs(int(input("b = ")))
-
+A, B = a, b
 while b > 0:
     re = r(a, b)
     print("{} = {} * {} + {}".format(a, b, q(a, b), r(a, b)))
     a, b = b, re
 
-print("\nPGCD({},{}) = {}".format(a,b,pgcd(a,b)))
+print("\nPGCD({},{}) = {}".format(A, B, pgcd(A, B)))
