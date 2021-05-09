23e48ab416dc31648ece161e71b0a58c15869d8d
unknown <kretzvictor@orange.fr>
2021-05-09 18:42:42 +0200

    Amélioration de prime(n)

--- scripts/maths/mathsup.py
+++ scripts/maths/mathsup.py
@@ -93,14 +93,18 @@ def ppcm(a, b):
 
 
 def prime(n):
+    L = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
+    103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 
+    223,227, 229, 233, 239, 241, 251]
     n = abs(n)
     if n == 1:
         return False
-    if n % 2 == 0:
-        if n == 2:
-            return True
-        return False
-    for i in range(3, round(n**0.5)+1, 2):
+    for k in L:
+        if n % k == 0:
+            if n == k:
+                return True
+            return False
+    for i in range(101, int(n**0.5)+1, 2):
         if n % i == 0:
             return False
     return True
