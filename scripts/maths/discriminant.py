fc1e2bba621ecac0e476771f112ba71e3561d77e
unknown <kretzvictor@orange.fr>
2021-05-11 16:26:53 +0200

    Update discriminant, prise en charge de valeur autre que float/int (sqrt(3) par ex)

--- scripts/maths/discriminant.py
+++ scripts/maths/discriminant.py
@@ -1,22 +1,27 @@
 from mathsup import disc
-from math import sqrt
+from math import *
 
 
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
+a = float(eval(input("a = ")))
+b = float(eval(input("b = ")))
+c = float(eval(input("c = ")))
 
 d = disc(a, b, c)
-print("\n d = " + str(d))
+print("\nd = " + str(d))
 if (d > 0):
-    print("x1 = (" + str(-b) + "-sqrt(" + str(d) + "))/(2*" + str(a) + ")\n= " + str((-b-sqrt(d))/(2*a)))
-    print("x2 = (" + str(-b) + "+sqrt(" + str(d) + "))/(2*" + str(a) + ")\n= " + str((-b+sqrt(d))/(2*a)))
-elif d==0:
-    print("x = " + str(-b) + "/(2*" + str(a) + ")\n= " + str(-b/(2*a)))
+    print("x1 = (" + str(-round(b, 2)) + "-sqrt(" + str(round(d, 2)) + "))/(2*" +
+          str(round(a, 2)) + ")\n= " + str(round((-b-sqrt(d))/(2*a), 2)))
+    print("x2 = (" + str(-round(b, 2)) + "+sqrt(" + str(round(d, 2)) + "))/(2*" +
+          str(round(a, 2)) + ")\n= " + str(round((-b+sqrt(d))/(2*a), 2)))
+elif d == 0:
+    print("x = " + str(-round(b, 2)) +
+          "/(2*" + str(round(a, 2)) + ")\n= " + str(-b/(2*a)))
 elif (d < 0):
     z1 = complex((-b/(2*a)), -(sqrt(-d))/(2*a))
-    alge1 = round(z1.real, 1) + round(z1.imag, 1) * 1j
-    print("z1 = (" + str(-b) + "-i*sqrt(" + str(-d) + "))/(2*" + str(a) + ")\n= " + str(alge1).replace("j", "i"))
+    alge1 = round(z1.real, 2) + round(z1.imag, 2) * 1j
+    print("z1 = (" + str(-round(b, 2)) + "-i*sqrt(" + str(-round(d, 2)) +
+          "))/(2*" + str(round(a, 2)) + ")\n= " + str(alge1).replace("j", "i"))
     z2 = complex((-b/(2*a)), -(sqrt(-d))/(2*a))
-    alge2 = round(z2.real, 1) + round(z2.imag, 1) * 1j
-    print("z2 = (" + str(-b) + "+i*sqrt(" + str(-d) + "))/(2*" + str(a) + ")\n= " + str(alge2).replace("j", "i"))
+    alge2 = round(z2.real, 2) + round(z2.imag, 2) * 1j
+    print("z2 = (" + str(-round(b, 2)) + "+i*sqrt(" + str(-round(d, 2)) +
+          "))/(2*" + str(round(a, 2)) + ")\n= " + str(alge2).replace("j", "i"))
